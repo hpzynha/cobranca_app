b64feb29abb4181f247ac028223b97feba2a9f27
hpzynha <larissa.nogueira.rocha@gmail.com>
2026-03-03 19:41:40 -0300

    fixing student page

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'dart:async';
 import 'package:app_cobranca/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/router/app_router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('pt_BR');
 
   await Supabase.initialize(
     url: 'https://frwxnviutavcycklveex.supabase.co',
